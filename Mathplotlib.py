ec1cc3eb12fb9e1f42b29d11a5b7842472be131a
KuyaAC <aciesnet02@gmail.com>
2026-08-09 23:55:40 +0800

    customizing plot

--- Mathplotlib.py
+++ Mathplotlib.py
@@ -25,3 +25,56 @@ plt.plot(gdp_cap, life_exp)
 
 # Display the plot
 plt.show()
+
+# Using Scatter Plots
+import matplotlib.pyplot as plt
+
+plt.scatter(gdp_cap, life_exp)
+plt.show()
+
+
+# Histograms
+# Create histogram of life_exp data
+plt.hist(life_exp)
+
+# Display histogram
+plt.show()
+
+# exercise 2(histograms)
+# Build histogram with 5 bins
+plt.hist(life_exp, bins=5)
+
+# Show and clean up plot and to start fresh again
+plt.show()
+plt.clf()
+
+# Build histogram with 20 bins
+plt.hist(life_exp, bins=20)
+
+# Show and clean up again
+plt.show()
+plt.clf()
+
+# Customizing Plots
+# Basic scatter plot, log scale
+plt.scatter(gdp_cap, life_exp)
+plt.xscale('log') 
+
+# Strings
+xlab = 'GDP per Capita [in USD]'
+ylab = 'Life Expectancy [in years]'
+title = 'World Development in 2007'
+
+# Add axis labels
+plt.xlabel(xlab)
+plt.ylabel(ylab)
+
+
+# Add title
+plt.title(title)
+
+# After customizing, display the plot
+plt.show()
+
+
+
