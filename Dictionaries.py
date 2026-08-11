34b2cd6a55a7f57e2c7ea6024933f8b450b1b0a7
KuyaAC <aciesnet02@gmail.com>
2026-08-11 16:57:28 +0800

    pandas part one done

--- Dictionaries.py
+++ Dictionaries.py
@@ -171,3 +171,24 @@ cars.index = row_labels
 # Print cars again
 print(cars)
 
+# Importing Data from a CSV
+# Import pandas as pd
+import pandas as pd
+
+# Import the cars.csv data: cars
+cars = pd.read_csv('cars.csv')
+
+# Print out cars
+print(cars)
+
+# Set index_col to 0 to use the first column as row labels
+# Import pandas as pd
+import pandas as pd
+
+# Fix import by including index_col
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Print out cars
+print(cars)
+
+
