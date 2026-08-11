5cc50ec2e8e7725c02c08272378ae06cd6be417d
KuyaAC <aciesnet02@gmail.com>
2026-08-11 16:35:13 +0800

    done dict

--- Dictionaries.py
+++ Dictionaries.py
@@ -110,3 +110,26 @@ del europe['australia']
 
 # Print europe
 print(europe)
+
+# DICTIONARY OF DICTIONARIES
+europe = { 'spain': { 'capital':'madrid', 'population':46.77 },
+           'france': { 'capital':'paris', 'population':66.03 },
+           'germany': { 'capital':'berlin', 'population':80.62 },
+           'norway': { 'capital':'oslo', 'population':5.084 } }
+
+
+# Print out the capital of France
+print(europe['france']['capital'])
+
+# Create sub-dictionary data
+data = {'capital':'rome', 'population':59.83}
+
+# Add data to europe under key 'italy'
+europe['italy'] = data
+
+# Print europe
+print(europe)
+
+# PANDAS(high level data manipulation tool built on top of NumPy)
+
+
