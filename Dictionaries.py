2c898a528f91bfb303d717cccd2b575a9abedc12
KuyaAC <aciesnet02@gmail.com>
2026-08-19 19:53:09 +0800

    Done with challenges

--- Dictionaries.py
+++ Dictionaries.py
@@ -250,3 +250,53 @@ print(cars[['country']])
 
 # Print out DataFrame with country and drives_right columns
 print(cars[['country', 'drives_right']])
+
+# Challenge 2:
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Print out first 3 observations
+print(cars[0:3])
+
+# Print out fourth, fifth and sixth observation
+print(cars[3:6])
+
+# Challenge 3: (loc and iloc)
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Print out observation for Japan
+print(cars.loc['JPN'])
+print(cars.iloc[2])
+# Print out observations for Australia and Egypt
+print(cars[:])
+print(cars.loc[['AUS', 'EG']])
+
+# Challenge 4:
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Print out drives_right value of Morocco
+print(cars.loc['MOR'],['drives_right'])
+
+# Print sub-DataFrame
+# print(cars[:])
+print(cars.loc[['RU', 'MOR'], ['country', 'drives_right']])
+
+# Challenge 5:
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Print out drives_right column as Series
+print(cars.loc[:, 'drives_right'])
+
+# Print out drives_right column as DataFrame
+print(cars.loc[:, ['drives_right']])
+
+# Print out cars_per_cap and drives_right as DataFrame
+print(cars.loc[:, ['cars_per_cap', 'drives_right']])
+
