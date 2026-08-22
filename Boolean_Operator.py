d970d1e37337ed74baa5f50bbcfbf5a008e142b0
KuyaAC <aciesnet02@gmail.com>
2026-08-23 04:44:12 +0800

    practice challenge

--- Boolean_Operator.py
+++ Boolean_Operator.py
@@ -17,5 +17,55 @@ print(day_off)  # True
 
 # not: reverses a Boolean value.
 is_raining = False
-can_play_outside = not is_raining
+can_play_outside = np.logical_not(is_raining)
 print(can_play_outside)  # True
+
+
+# Functions that demonstrate the same logical operations.
+def logical_and(first, second):
+	"""Return True only when both values are true."""
+	return np.logical_and(first, second)
+
+
+def logical_or(first, second):
+	"""Return True when at least one value is true."""
+	return np.logical_or(first, second)
+
+
+def logical_not(value):
+	"""Return the opposite Boolean value."""
+	return np.logical_not(value)
+
+
+print(logical_and(True, False))  # False: both values are not true.
+print(logical_or(False, True))   # True: at least one value is true.
+print(logical_not(False))        # True: not False is True.
+
+
+# Challenge 1:
+# Define variables
+my_kitchen = 18.0
+your_kitchen = 14.0
+
+# my_kitchen bigger than 10 and smaller than 18?
+print(my_kitchen > 10 and my_kitchen < 18)
+
+# my_kitchen smaller than 14 or bigger than 17?
+print(my_kitchen < 14 or my_kitchen > 17)
+
+# Double my_kitchen smaller than triple your_kitchen?
+print((my_kitchen*2) < (your_kitchen*3))
+
+
+#Challenge 2:
+# Create arrays
+import numpy as np
+my_house = np.array([18.0, 20.0, 10.75, 9.50])
+your_house = np.array([14.0, 24.0, 14.25, 9.0])
+
+# my_house greater than 18.5 or smaller than 10
+print(np.logical_or(my_house > 18.5, my_house < 10))
+
+# Both my_house and your_house smaller than 11
+print(np.logical_and(my_house < 11, your_house < 11))
+
