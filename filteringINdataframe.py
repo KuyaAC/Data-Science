3586a53e6ea0c3c12bf0e592dee79b57d5b3c53a
KuyaAC <aciesnet02@gmail.com>
2026-08-24 04:17:05 +0800

    Done this C2

--- filteringINdataframe.py
+++ filteringINdataframe.py
@@ -52,3 +52,21 @@ car_maniac = cars[cars['cars_per_cap'] > 500]
 
 # Print car_maniac
 print(car_maniac)
+
+# Challenge 4:
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Import numpy, you'll need this
+import numpy as np
+
+# Create medium: observations with cars_per_cap between 100 and 500
+cpc = cars['cars_per_cap']
+between = np.logical_and(cpc > 100, cpc < 500)
+medium = cars[between]
+
+
+
+# Print medium
+print(medium)
