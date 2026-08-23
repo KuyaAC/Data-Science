3af9d504d7e26c6d9d6edecc04a78d48d7abc354
KuyaAC <aciesnet02@gmail.com>
2026-08-24 04:05:34 +0800

    Challengei 1

--- filteringINdataframe.py
+++ filteringINdataframe.py
@@ -4,3 +4,16 @@ import numpy as np
 np.logical_and(brics['area'] > 8, brics['area'] < 10) 
 brics[np.logical_and(brics['area'] > 8, brics['area'] < 10)]
 
+# Challenge 1:
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Extract drives_right column as Series: dr
+dr = cars['drives_right']
+
+# Use dr to subset cars: sel
+sel = cars[dr]
+
+# Print sel
+print(sel)
