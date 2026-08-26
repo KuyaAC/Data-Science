8f24306389c2efc035206f675291cfe253f38312
KuyaAC <aciesnet02@gmail.com>
2026-08-26 17:27:54 +0800

    done part 1

--- loop_ds.py
+++ loop_ds.py
@@ -29,7 +29,16 @@ for x, y in europe.items():
     print("the capital of " + x +" is " + y)
 
 # Challenge 2:
+# Import numpy as np
+import numpy as np
 
+# For loop over np_height
+for x in np_height:
+    print(str(x) + " inches")
+
+# For loop over np_baseball
+for x in np.nditer(np_baseball):
+    print(x)
 
 
 
