ebb76f08ec719272df61da8aec843b404d131e9a
KuyaAC <aciesnet02@gmail.com>
2026-09-05 16:10:13 +0800

    plotting random walks

--- Intermediate Python/Random.py
+++ Intermediate Python/Random.py
@@ -41,7 +41,7 @@ print(dice2)
 # Else, you throw the dice again. The number on the dice is the number of steps you go up.
 # Print out dice and step. Given the value of dice, was step updated correctly?
 
-# Code:
+# Solution:
 # NumPy is imported, seed is set
 
 # Starting step
@@ -62,4 +62,66 @@ else :
 print(dice)
 print(step)
 
+# instruction:
+# Make a list random_walk that contains the first step, which is the integer 0.
+# Finish the for loop:
+# The loop should run 100 times.
+# On each iteration, set step equal to the last element in the random_walk list. You can use the index -1 for this.
+# Next, let the if-elif-else construct update step for you.
+# The code that appends step to random_walk is already coded.
+# Print out random_walk.
 
+# Solution:
+# NumPy is imported, seed is set
+
+# Initialize random_walk
+random_walk = [0]
+
+# Complete the ___
+for x in range(100) :
+    # Set step: last element in random_walk
+    step = random_walk[-1]
+
+    # Roll the dice
+    dice = np.random.randint(1,7)
+
+    # Determine next step
+    if dice <= 2:
+        step = step - 1
+    elif dice <= 5:
+        step = step + 1
+    else:
+        step = step + np.random.randint(1,7)
+
+    # append next_step to random_walk
+    random_walk.append(step)
+
+# Print random_walk
+print(random_walk)
+
+# Intruction:
+# Use max() in a similar way to make sure that step doesn't go below zero if dice <= 2.
+# Hit Submit Answer and check the contents of random_walk.
+
+
+# Solution:
+# NumPy is imported, seed is set
+
+# Initialize random_walk
+random_walk = [0]
+
+for x in range(100) :
+    step = random_walk[-1]
+    dice = np.random.randint(1,7)
+
+    if dice <= 2:
+        # Replace below: use max to make sure step can't go below 0
+        step = max(0, step - 1)
+    elif dice <= 5:
+        step = step + 1
+    else:
+        step = step + np.random.randint(1,7)
+
+    random_walk.append(step)
+
+print(random_walk)
