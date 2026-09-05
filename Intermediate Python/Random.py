f77157532834fed0a0d8a3819199fe64c6faa188
KuyaAC <aciesnet02@gmail.com>
2026-09-05 15:48:01 +0800

    random dice complete

--- Intermediate Python/Random.py
+++ Intermediate Python/Random.py
@@ -19,4 +19,47 @@ rannum = np.random.rand()
 # Generate and print random float
 print(rannum)
 
+# ----------------------------------------- Roll a dice challenge -----------------------------------------------
+# Challenge 1: roll a dice
+# Import numpy and set seed
+import numpy as np
+np.random.seed(123)
+
+# Use randint() to simulate a dice
+dice = np.random.randint(1,7)
+dice2 = np.random.randint(1,7)
+# Use randint() again
+print(dice)
+print(dice2)
+
+
+# Instruction:
+# Roll the dice. Use randint() to create the variable dice.
+# Finish the if-elif-else construct by replacing ___:
+# If dice is 1 or 2, you go one step down.
+# if dice is 3, 4 or 5, you go one step up.
+# Else, you throw the dice again. The number on the dice is the number of steps you go up.
+# Print out dice and step. Given the value of dice, was step updated correctly?
+
+# Code:
+# NumPy is imported, seed is set
+
+# Starting step
+step = 50
+
+# Roll the dice
+dice = np.random.randint(1,7)
+
+# Finish the control construct
+if dice <= 2 :
+    step = step - 1
+elif dice <= 5 and dice >= 3:
+    step = step + 1
+else :
+    step = step + np.random.randint(1,7)
+
+# Print out dice and step
+print(dice)
+print(step)
+
 
